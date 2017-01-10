d0fe4e3ded6cde7b691fa8f293c0e4eedeb0b13c
authorwang <wang19970119@outlook.com>
2017-01-10 11:06:01 +0800

    Rename CheckIn to Home Established PPTFragment finished PPTFragment BaseUI complete PPTFragment BaseFunction

--- app/src/main/java/com/ant/nepu/teachent/adapter/PPTListAdapter.java
+++ app/src/main/java/com/ant/nepu/teachent/adapter/PPTListAdapter.java
@@ -5,9 +5,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import com.ant.nepu.teachent.R;
+
+import java.util.HashMap;
 
 /**
  * 课件ListView自定义适配器
@@ -15,15 +18,31 @@ import com.ant.nepu.teachent.R;
  */
 
 public class PPTListAdapter extends BaseAdapter {
-    private String[] texts;
-    private Context context;
+
+    public static HashMap<Integer,Boolean> isSelected;//checkbox选中状态
+    private String[] texts;//数据显示文本数组
+    private Context context;//上下文
+
 //    private int[] icons;
 
     public PPTListAdapter(Context context, String[] texts){
         this.context = context;
         this.texts = texts;
+        isSelected = new HashMap<>();
+
+        initCheckBoxState();//初始化checkbox状态
 //        this.icons = icons;
     }
+
+    /**
+     * 初始化checkbox状态
+     */
+    private void initCheckBoxState() {
+        for(int i=0;i<texts.length;i++){
+            isSelected.put(i,false);//默认未选中
+        }
+    }
+
     @Override
     public int getCount() {
         return texts.length;
@@ -40,13 +59,14 @@ public class PPTListAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         ViewHolder vh;
         if(convertView==null){
             LayoutInflater inflater = LayoutInflater.from(context);
             convertView = inflater.inflate(R.layout.ppt_list_item,null);
             vh = new ViewHolder();
             vh.tv_text = (TextView) convertView.findViewById(R.id.tv_frag_ppt_text);
+            vh.cb = (CheckBox) convertView.findViewById(R.id.cb_frag_ppt);
             convertView.setTag(vh);
         }else{
             vh = (ViewHolder) convertView.getTag();
@@ -56,6 +76,24 @@ public class PPTListAdapter extends BaseAdapter {
 //        TextView tv_text =(TextView) convertView.findViewById(R.id.tv_frag_ppt_text);
 //        TextView tv_text =(TextView) convertView.findViewById(R.id.tv_frag_ppt_text);
         vh.tv_text.setText(texts[position]);
+        /**
+         * checkbox监听
+         */
+        vh.cb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isSelected.get(position)){
+                    isSelected.put(position,false);
+                }else{
+                    isSelected.put(position,true);
+                }
+            }
+        });
+
+        /**
+         * 设置checkbox状态
+         */
+        vh.cb.setChecked(isSelected.get(position));
         return convertView;
     }
 
@@ -64,5 +102,6 @@ public class PPTListAdapter extends BaseAdapter {
      */
     static class ViewHolder{
         TextView tv_text;
+        CheckBox cb;
     }
 }
--- app/src/main/java/com/ant/nepu/teachent/fragment/PPTFragment.java
+++ app/src/main/java/com/ant/nepu/teachent/fragment/PPTFragment.java
@@ -7,11 +7,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.ant.nepu.teachent.R;
 import com.ant.nepu.teachent.adapter.PPTListAdapter;
 import com.ant.nepu.teachent.common.CommonData;
+
+import java.util.HashMap;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,6 +28,8 @@ public class PPTFragment extends Fragment {
     View mView;
     private ListView listView;
     private PPTListAdapter pptListAdapter;
+    private Button btnDownload;
+    private String result;//测试：download获取信息
 
     public PPTFragment() {
 
@@ -33,21 +39,36 @@ public class PPTFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        result = "";//测试显示结果置空
+
         // Inflate the layout for this fragment
         mView = inflater.inflate(R.layout.fragment_ppt, container, false);
 
         //findView
         listView = (ListView) mView.findViewById(R.id.lv_frag_ppt);
+        btnDownload = (Button) mView.findViewById(R.id.btn_frag_ppt_download);
 
         //setOnClickListener
+
         /**
-         * ListView项点击监听事件
+         * 下载按钮点击监听事件
          */
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        btnDownload.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public void onClick(View v) {
+                HashMap<Integer,Boolean> isSelected = PPTListAdapter.isSelected;
+                for(int i=0;i<pptListAdapter.getCount();i++){
+                    if(isSelected.get(i)){
+                        String text = (String) pptListAdapter.getItem(i);
+                        result+="\n"+text;
+                    }
+                }
+                Toast.makeText(mView.getContext(),result,Toast.LENGTH_SHORT).show();
 
             }
+
+
         });
 
         //加载listview数据
@@ -61,10 +82,10 @@ public class PPTFragment extends Fragment {
     private void loadData() {
        //测试：数据
         String[] texts = {"大学英语一级Chapter1",
-                "大学英语一级Chapter1",
                 "大学英语一级Chapter2",
                 "大学英语一级Chapter3",
                 "大学英语一级Chapter4",
+                "大学英语一级Chapter5",
                 "大学英语二级Chapter1",
                 "大学英语二级Chapter2",
                 "大学英语二级Chapter3",
