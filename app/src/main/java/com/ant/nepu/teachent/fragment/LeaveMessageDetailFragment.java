ff89395849deb788e478b6ed3c8f7011109c4aaf
WMS <wang19970119@outlook.com>
2017-01-23 22:43:06 +0800

    finished leave message fragment

--- app/src/main/java/com/ant/nepu/teachent/fragment/LeaveMessageDetailFragment.java
+++ app/src/main/java/com/ant/nepu/teachent/fragment/LeaveMessageDetailFragment.java
@@ -8,8 +8,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ant.nepu.teachent.R;
+import com.ant.nepu.teachent.common.CommonData;
 
 import org.w3c.dom.Text;
 
@@ -37,7 +39,39 @@ public class LeaveMessageDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_leave_message_detail, container, false);
+        mView =  inflater.inflate(R.layout.fragment_leave_message_detail, container, false);
+
+        //findViews
+        btn_back = (Button) mView.findViewById(R.id.btn_frag_leave_message_detail_close);
+        btn_comment = (Button) mView.findViewById(R.id.btn_frag_leave_message_detail_comment);
+        tv_text = (TextView) mView.findViewById(R.id.tv_frag_leave_message_detail_text);
+        tv_comment = (TextView) mView.findViewById(R.id.tv_frag_leave_message_detail_comment);
+
+        //加载数据
+        loadData();
+
+        //onClickListeners
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    getFragmentManager().beginTransaction().replace(R.id.content_teachent_main,new LeaveMessageFragment()).commit();
+            }
+        });
+
+        btn_comment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(),"评论提交",Toast.LENGTH_SHORT).show();
+            }
+        });
+        return mView;
+    }
+
+    /**
+     * 加载留言板详情
+     */
+    private void loadData() {
+        tv_text.setText(CommonData.leaveMessageDetailTextList[CommonData.leaveMessagePosition]);
     }
 
 }
--- app/src/main/java/com/ant/nepu/teachent/fragment/LeaveMessageFragment.java
+++ app/src/main/java/com/ant/nepu/teachent/fragment/LeaveMessageFragment.java
@@ -84,8 +84,25 @@ public class LeaveMessageFragment extends Fragment {
                 "你对我很好，很爱我，很呵护我，我谢谢你...",
         };
 
+        String[] leaveMessageDetailTextList = {
+
+                "有山无水难成景，有酒无朋难聚欢；曾经沧海成桑田，情意交心亘不变；红梅飘香话思念，惹落雪花两三片；大好时光多缠绵，愿你快乐每一天！",
+                "露水的晶莹是对你的眷恋，霜花的曼妙是对你的期盼，飞雪的覆盖是欣慰一年的平安，亲爱的朋友，友谊的长久牵着流年，情意的甘甜陈酿青春的诺言，祝愿友谊地久天长。",
+                "用微笑装作不在意你的嘲笑，不关心你的离去。",
+                "孤单时，友谊是一柄利剑，助你披荆斩棘；迷茫时，朋友是一盏明灯，为你点亮心灵；疲乏时，祝福是一弯山泉，让你净化心灵。每天把好运装满，开心前行。",
+                "别把分手说得那么好听，再怎么好听心还是会痛的。",
+                "我很想告诉你。我好想你。可是我怕得到的只是一个简单的回答“哦”字。",
+                "没有经历过的人，没有对于他的意义。",
+                "剪不断，理还乱，是离愁。",
+                "妄想去留住原本应该消失的人和事，其实是一种悲剧。",
+                "因为不曾相识，所以也不曾悲伤。更不曾快乐过。",
+                "有时候，放弃一些东西才能得到一些什么，得到了以后又想再得到放弃的那些。",
+                "你对我很好，很爱我，很呵护我，我谢谢你。但不知道从什么时候开始，你就慢慢地不在乎我了，我到底做错什么了，以至于你不要我了？我可是你的，脸啊！",
+        };
+
         CommonData.leaveMessageNameList = leaveMessageNameList;
         CommonData.leaveMessageSimpleTextList = leaveMessageSimpleTextList;
+        CommonData.leaveMessageDetailTextList = leaveMessageDetailTextList;
 
         leaveMessageListAdapter = new LeaveMessageListAdapter(mView.getContext(),leaveMessageNameList,leaveMessageSimpleTextList,this);
         recyclerView.setLayoutManager(new LinearLayoutManager(mView.getContext()));
