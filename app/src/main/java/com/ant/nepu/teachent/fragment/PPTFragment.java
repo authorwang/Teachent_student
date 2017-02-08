2754e5138c4e869ebf46da5523e8165f7f48e27e
WMS <wang19970119@outlook.com>
2017-02-08 22:58:45 +0800

    finish login function finish basic loading constructing initial activity finish initial no and name finsh initial function all partly checkin function need to consider plus number problem finished checkin function (lack for avatar) finished ppt base function add loading dialogs finished ppt function

--- app/src/main/java/com/ant/nepu/teachent/fragment/PPTFragment.java
+++ app/src/main/java/com/ant/nepu/teachent/fragment/PPTFragment.java
@@ -2,6 +2,7 @@ package com.ant.nepu.teachent.fragment;
 
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v4.app.Fragment;
@@ -9,6 +10,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -19,11 +21,18 @@ import com.ant.nepu.teachent.common.Constants;
 import com.ant.nepu.teachent.dialog.DownloadingDialog;
 import com.ant.nepu.teachent.dialog.LoadingDialog;
 import com.ant.nepu.teachent.util.AVCloudUtils;
+import com.ant.nepu.teachent.util.FileUtils;
 import com.avos.avoscloud.AVCloudQueryResult;
 import com.avos.avoscloud.AVException;
+import com.avos.avoscloud.AVFile;
 import com.avos.avoscloud.AVQuery;
 import com.avos.avoscloud.CloudQueryCallback;
+import com.avos.avoscloud.GetDataCallback;
+import com.avos.avoscloud.ProgressCallback;
+import com.avos.avoscloud.okio.ByteString;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -58,7 +67,7 @@ public class PPTFragment extends Fragment {
         loadingDialog.show();
         Handler handler = new Handler(){
             @Override
-            public void handleMessage(Message msg) {
+            public void handleMessage(final Message msg) {
                 super.handleMessage(msg);
                 switch (msg.what){
                     case Constants.PRE_DATA_PREPARED:
@@ -67,6 +76,52 @@ public class PPTFragment extends Fragment {
                     case Constants.DATA_PREPARED:
                         pptListAdapter = new PPTListAdapter(mView.getContext());
                         listView.setAdapter(pptListAdapter);
+                        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+                                final DownloadingDialog downloadingDialog = new DownloadingDialog(mView.getContext());
+                                downloadingDialog.show();
+                                String targetPPTId = CommonData.pptIdList.get(position);
+                                String pptFileCql = "select include pptfile from ppt where pid='"+targetPPTId+"'";
+                                AVQuery.doCloudQueryInBackground(pptFileCql, new CloudQueryCallback<AVCloudQueryResult>() {
+                                    @Override
+                                    public void done(AVCloudQueryResult avCloudQueryResult, AVException e) {
+                                        if(e==null){
+                                            AVFile file = avCloudQueryResult.getResults().get(0).getAVFile("pptfile");
+                                            file.getDataInBackground(new GetDataCallback() {
+                                                @Override
+                                                public void done(byte[] bytes, AVException e) {
+                                                    String path = Environment.getExternalStorageDirectory().getAbsolutePath()+
+                                                            "/"+"TeachentFiles/PPTFiles/"
+                                                            +CommonData.pptNameList.get(position)+".ppt";
+
+                                                    String fileName = Environment.getExternalStorageDirectory().getAbsolutePath() +
+                                                            File.separator + "TeachentFiles" +
+                                                            File.separator+"PPTFiles";
+                                                    File directory = new File(fileName);
+                                                    directory.mkdirs();
+
+                                                    File file = FileUtils.getFileFromBytes(bytes,path);
+                                                    try {
+                                                   file.createNewFile();
+                                                    } catch (IOException e1) {
+                                                        e1.printStackTrace();
+                                                    }
+                                                }
+                                            }, new ProgressCallback() {
+                                                @Override
+                                                public void done(Integer integer) {
+                                                    if(integer==100){
+                                                        downloadingDialog.dismiss();
+                                                        Toast.makeText(mView.getContext(),"下载完成！",Toast.LENGTH_SHORT).show();
+                                                    }
+                                                }
+                                            });
+                                        }
+                                    }
+                                });
+                            }
+                        });
                         loadingDialog.dismiss();
                         break;
                 }
---
+++ app/src/main/java/com/ant/nepu/teachent/util/FileUtils.java
@@ -0,0 +1,49 @@
+package com.ant.nepu.teachent.util;
+
+import android.os.Environment;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * 文件操作工具类
+ * Created by WMS on 2017/2/8.
+ */
+
+public class FileUtils {
+
+
+    /**
+     * 把字节数组保存为一个文件
+     *
+     * @param b
+     * @param outputFile
+     * @return
+     */
+    public static File getFileFromBytes(byte[] b, String outputFile) {
+        File ret = null;
+        BufferedOutputStream stream = null;
+        try {
+            ret = new File(outputFile);
+            FileOutputStream fstream = new FileOutputStream(ret);
+            stream = new BufferedOutputStream(fstream);
+            stream.write(b);
+        } catch (Exception e) {
+            // log.error("helper:get file from byte process error!");
+            e.printStackTrace();
+        } finally {
+            if (stream != null) {
+                try {
+                    stream.close();
+                } catch (IOException e) {
+                    // log.error("helper:get file from byte process error!");
+                    e.printStackTrace();
+                }
+            }
+        }
+        return ret;
+    }
+}
